b187bf3d9b2f5bda6bda107118f15b0b44c2f6fe
Patrick R. Wright <patrick.wright@usb.ch>
2018-09-12 14:28:11 +0200

    first src code commit

---
+++ secuTrial/R/R/secuTrial-package.R
@@ -0,0 +1,8 @@
+#' secuTrial package
+#'
+#' This R package provides functions for handling data
+#' from the clinical data management system secuTrial.
+#'
+#' @name secuTrial
+#' @docType package
+NULL

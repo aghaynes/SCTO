cc9c49091d7206ae6d29fedbc43edd280a7648ad
Patrick R. Wright <patrick.wright@usb.ch>
2018-09-20 10:45:11 +0200

    finish dir restructuring

--- secuTrial/R/R/secuTrial-package.R
+++
@@ -1,8 +0,0 @@
-#' secuTrial package
-#'
-#' This R package provides functions for handling data
-#' from the clinical data management system secuTrial.
-#'
-#' @name secuTrial
-#' @docType package
-NULL
